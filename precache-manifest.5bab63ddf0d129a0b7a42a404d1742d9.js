8c720dc82d19fde3cc92ff345fa3ea4fd2bca405
fieedw <frankding8843@gmail.com>
2020-06-29 20:04:44 +0800

    deploy

---
+++ precache-manifest.5bab63ddf0d129a0b7a42a404d1742d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025a8812a19981492a14",
+    "url": "/oilRice-money1/css/app.fc5a35a8.css"
+  },
+  {
+    "revision": "da27b7087b69cb423c0401048b789ee6",
+    "url": "/oilRice-money1/index.html"
+  },
+  {
+    "revision": "025a8812a19981492a14",
+    "url": "/oilRice-money1/js/app.42bccd6d.js"
+  },
+  {
+    "revision": "7716c195ccd91595b01d",
+    "url": "/oilRice-money1/js/chunk-vendors.4e887dfd.js"
+  },
+  {
+    "revision": "f3aaee380deddea3de78c3de33e81ce5",
+    "url": "/oilRice-money1/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/oilRice-money1/robots.txt"
+  }
+]);
